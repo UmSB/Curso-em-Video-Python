54a5108286423f47f2bb665fcc880a8f6e4cb744
Thiago A. Carvalho <118239712+Thiago-A-Carvalho@users.noreply.github.com>
2024-08-07 23:32:45 -0300

    Update ex023.py
    Ajuste

--- aula08/ex023.py
+++ aula08/ex023.py
@@ -2,8 +2,8 @@
 Faça um programa em Python que abra e reproduza o áudio de um MP3.
 """
 import pygame
-pygame.mixer.init()
+pygame.init()
 pygame.mixer.music.load('ex023.mp3')
 pygame.mixer.music.play()
-input('Aguarde a musica inicar')
+input()
 pygame.event.wait()
